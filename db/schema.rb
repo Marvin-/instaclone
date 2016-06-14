7892dccf20044b8b63f3297270e6c3dc73ced008
Marvin M <marvin23m@gmail.com>
2016-06-14 16:55:43 +0100

    Add user id column to Pics table

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160614154747) do
+ActiveRecord::Schema.define(version: 20160614155438) do
 
-  create_table "pics", force: :cascade do |t|
-    t.string   "title"
-    t.text     "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-  end
+# Could not dump table "pics" because of following NoMethodError
+#   undefined method `[]' for nil:NilClass
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
